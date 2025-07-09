1fe4142ac673e3c0385ceffa0c2300b73b20b946
Prakash <agrawal.prakash@gmail.com>
2025-07-09 10:45:59 +0530

    saga pattern commit messages

---
+++ src/test/java/com/prakasha/saga_choreography_pattern/SagaChoreographyPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakasha.saga_choreography_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaChoreographyPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
